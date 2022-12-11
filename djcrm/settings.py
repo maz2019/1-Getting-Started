f61d13b331cfa0bc1d578011dc89ec44d77799e4
Mohamed Elazhary <azhary2@hotmail.com>
2022-12-11 21:02:55 +0200

    2121

--- djcrm/settings.py
+++ djcrm/settings.py
@@ -172,7 +172,7 @@ if not DEBUG:
     SECURE_HSTS_PRELOAD = True
     X_FRAME_OPTIONS = "DENY"
 
-    ALLOWED_HOSTS = ['127.0.0.1','10.244.13.37', '10.244.2.35']
+    ALLOWED_HOSTS = ['*']
 
     EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
     EMAIL_HOST = env("EMAIL_HOST")
