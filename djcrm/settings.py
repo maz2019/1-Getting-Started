75d897637cdfd8e9cc8deff8f43a275d8373f29f
Mohamed Elazhary <azhary2@hotmail.com>
2022-12-11 13:27:25 +0200

    simaplesolve

--- djcrm/settings.py
+++ djcrm/settings.py
@@ -159,4 +159,10 @@ if not DEBUG:
 
     ALLOWED_HOSTS = ["*"]
 
-TAILWIND_APP_NAME = 'theme'
+    EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+    EMAIL_HOST = env("EMAIL_HOST")
+    EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+    EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = env("EMAIL_PORT")
+    DEFAULT_FROM_EMAIL = env("DEFAULT_FROM_EMAIL")
--- runserver.sh
+++ runserver.sh
@@ -2,4 +2,5 @@ python manage.py collectstatic --no-input
 
 python manage.py migrate
 
+
 gunicorn --worker-tmp-dir /dev/shm djcrm.wsgi
